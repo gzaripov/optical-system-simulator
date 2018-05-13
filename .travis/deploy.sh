4a3865b922c708e32498559d8cbcf3c2a046dafa
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-05-13 17:36:46 +0300

    chore: update travis build script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+function bell() {
+  while true; do
+    echo -e "\a"
+    sleep 60
+  done
+}
 set -e
 git config --global push.default simple
 git remote add prod ssh://travis@gzaripov.com/~/optical-system-simulator/
-git push prod master
+bell & git push prod master
+exit $?
