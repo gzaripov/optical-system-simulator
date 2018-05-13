0b1b76efbfd809f3564b7ba2f001b4cfe3474450
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-05-13 16:52:05 +0300

    chore: update remote url

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,5 @@ set -e
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add prod ssh://travis@gzaripov.com/optical-system-simulator
+git remote add prod ssh://travis@gzaripov.com/~/optical-system-simulator/
 git push prod master # push our updates
