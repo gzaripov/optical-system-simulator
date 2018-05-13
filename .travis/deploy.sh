1716c5944953051b567da0f7e3df6f2db5947b55
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-05-13 17:14:41 +0300

    chore: remove comments from deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-git config --global push.default simple # we only want to push one branch — master
-# specify the repo on the live server as a remote repo, and name it 'production'
-# <user> here is the separate user you created for deploying
+git config --global push.default simple
 git remote add prod ssh://travis@gzaripov.com/~/optical-system-simulator/
-git push prod master # push our updates
+git push prod master
