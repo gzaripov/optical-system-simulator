89ea3c3858f5ada5051fe8c9614f001d9331d6d9
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-05-18 19:47:08 +0300

    refactor: refactror config-overrides due airbnb codestyle

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,8 @@
-const { injectBabelPlugin } = require("react-app-rewired");
+const { injectBabelPlugin } = require('react-app-rewired');
 
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-    ["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }],
-    config
+module.exports = function override(config) {
+  return injectBabelPlugin(
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+    config,
   );
-  return config;
 };
