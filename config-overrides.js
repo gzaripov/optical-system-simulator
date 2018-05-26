d52f773782aca775e33d1c0b6229d86c7f6f17a0
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-05-26 14:53:32 +0300

    chore: add babel-plugin-styled-components

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,17 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
-module.exports = function override(config) {
+function rewireStyledComponents(config, env, styledComponentsPluginOptions = {}) {
+  return injectBabelPlugin(['styled-components', styledComponentsPluginOptions], config);
+}
+
+function rewireAntd(config) {
   return injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
     config,
   );
+}
+
+module.exports = function override(config, env) {
+  const rewiredConfig = rewireStyledComponents(config, env);
+  return rewireAntd(rewiredConfig);
 };
