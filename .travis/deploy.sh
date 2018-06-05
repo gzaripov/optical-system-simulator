62025acebb98708a4e13453067f12b8ea6f7e3c4
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-06-05 22:43:31 +0300

    chore: update travis build script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 git config --global push.default simple
 git remote add prod ssh://travis@gzaripov.com/~/optical-system-simulator/
-git push prod master
+git push prod master --force
 ssh travis@gzaripov.com "cd ~/optical-system-simulator/; yarn install; yarn build"
