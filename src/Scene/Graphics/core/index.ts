74c64023cc2587dc3e7ae37b2d7dd9044fa52a64
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-06-26 21:39:35 +0300

    refactor: move core/index to typescript

---
+++ src/Scene/Graphics/core/index.ts
@@ -0,0 +1,19 @@
+import BoundsRenderer from "./BoundsRenderer";
+import colorBufferFloatTest from "./colorBufferFloatTest";
+import DragObserver from "./DragObserver";
+import Lens from "./Lens";
+import LightSource from "./LightSource";
+import Prism from "./Prism";
+import Renderer from "./Renderer";
+import SpectrumRenderer from "./SpectrumRenderer";
+
+export {
+  Renderer,
+  BoundsRenderer,
+  SpectrumRenderer,
+  DragObserver,
+  Lens,
+  Prism,
+  LightSource,
+  colorBufferFloatTest
+};
