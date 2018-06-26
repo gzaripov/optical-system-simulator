f2a444a2e493fc2e369f3bededf351822f3a12c6
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-06-26 22:02:09 +0300

    refactor: move BoundsRenderer to typescript

---
+++ src/Scene/Graphics/core/BoundsRenderer.ts
@@ -0,0 +1,19 @@
+import Drawable from "./Drawable";
+
+export default class BoundsRenderer {
+  private ctx: CanvasRenderingContext2D;
+
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx;
+  }
+
+  public clear() {
+    const { ctx } = this;
+    const { width, height } = this.ctx.canvas;
+    ctx.clearRect(0, 0, width, height);
+  }
+
+  public draw(drawable: Drawable) {
+    drawable.drawToCanvas(this.ctx);
+  }
+}
