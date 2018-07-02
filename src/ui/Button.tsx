0103c04e14e5b3250cdca9313353950bc17b9549
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-07-02 12:40:33 +0300

    refactor: move Button to typescript

---
+++ src/ui/Button.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import styled from "styled-components";
+import DefaultProps from "./DefaultProps";
+
+const ButtonStyled = styled.button`
+  padding: 0;
+  border: 0;
+  cursor: pointer;
+  background: none;
+`;
+
+interface Props extends DefaultProps {
+  onClick: () => void;
+}
+
+const Button: React.SFC<Props> = ({ children, onClick, className }) => (
+  <ButtonStyled type="button" onClick={onClick} className={className}>
+    {children}
+  </ButtonStyled>
+);
+
+Button.defaultProps = {
+  onClick: () => void 0
+};
+
+export default Button;
