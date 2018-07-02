fccebbfdceceb258fc5eb213c702b8a8521cd99a
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-07-02 11:29:25 +0300

    chore: add rewire-ts plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,39 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const { injectBabelPlugin, getLoader } = require("react-app-rewired");
+const tsImportPluginFactory = require("ts-import-plugin");
 
-function rewireStyledComponents(config, env, styledComponentsPluginOptions = {}) {
-  return injectBabelPlugin(['styled-components', styledComponentsPluginOptions], config);
+function rewireStyledComponents(
+  config,
+  env,
+  styledComponentsPluginOptions = {}
+) {
+  return injectBabelPlugin(
+    ["styled-components", styledComponentsPluginOptions],
+    config
+  );
 }
 
 function rewireAntd(config) {
-  return injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
-    config,
+  const tsLoader = getLoader(
+    config.module.rules,
+    rule =>
+      rule.loader &&
+      typeof rule.loader === "string" &&
+      rule.loader.includes("ts-loader")
   );
+
+  tsLoader.options = {
+    getCustomTransformers: () => ({
+      before: [
+        tsImportPluginFactory({
+          libraryDirectory: "es",
+          libraryName: "antd",
+          style: "css"
+        })
+      ]
+    })
+  };
+
+  return config;
 }
 
 module.exports = function override(config, env) {
